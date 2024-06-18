b1ad0a1cc2c9e2f48672d3716f87cc556129ab33
Fadhil Faqih <153825017+Faqyh@users.noreply.github.com>
2024-06-18 18:39:52 +0800

    Memberikan izin akses ke semua local web

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const port = 3000;
 
 // Middleware untuk mengizinkan CORS
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', true);
